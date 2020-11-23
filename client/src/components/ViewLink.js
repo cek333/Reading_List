7cccbb752cfd2dcb374df51be5ddb48ce85cf090
Cecil <69444825+cek333@users.noreply.github.com>
2020-11-22 21:24:10 -0500

    open links on new page

--- client/src/components/ViewLink.js
+++ client/src/components/ViewLink.js
@@ -1,5 +1,6 @@
 function ViewLink(props) {
-  return <a className="btn btn-outline-info float-right ml-1" {...props}>View</a>;
+  return <a className="btn btn-outline-info float-right ml-1"
+            target="_blank" rel="noopener noreferrer" {...props}>View</a>;
 }
 
 export default ViewLink;
