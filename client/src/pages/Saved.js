68ddc97005e80ed4d797c64ef0ceda6d405d58fa
Cecil <69444825+cek333@users.noreply.github.com>
2020-12-18 20:22:43 -0500

    delay updating display until api call complete

--- client/src/pages/Saved.js
+++ client/src/pages/Saved.js
@@ -6,18 +6,29 @@ import { getBooks, deleteBook } from '../utils/API';
 function Saved() {
   const [savedList, setSavedList] = useState([]);
 
+  // 'No results found' is temporarily displayed before API call completes,
+  //   causing an on-screen glitch.
+  //   Use showList to suppress 'No results found' until API call complete.
+  const [showList, setShowList] = useState(false);
+
   useEffect(function() {
-    getBooks(setSavedList);
+    getBooks((res) => {
+      setSavedList(res);
+      setShowList(true);
+    });
   }, []);
 
   function handleClick(id) {
-    deleteBook(id);
-    getBooks(setSavedList);
+    deleteBook(id, (res)=> {
+      // When delete operation complete, get updated list of books
+      getBooks(setSavedList);
+    });
   }
 
   return (
     <ListContainer>
-      {savedList.length===0
+      {showList &&
+        savedList.length===0
         ? <p>No results found.</p>
         : savedList.map(book => <ListItem onClick={() => handleClick(book._id)} btnAction="Delete"
                                           disabled={false} key={book._id} item={book} />)
