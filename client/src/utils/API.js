cea28904c64890bf820a739bdad891a9a1fb0cd0
Cecil <69444825+cek333@users.noreply.github.com>
2021-07-09 18:45:04 -0400

    refactor to reduce code duplication

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -1,52 +1,36 @@
-function searchBooks(query, cb) {
-  fetch(`/api/googlebooks?q=${query}`)
+function fetchJSON(url, cb=noop, method='get', data={}) {
+  let settings = {
+    method,
+    headers: { 'Content-Type': 'application/json' }
+  };
+  if (method === 'post' || method === 'put') {
+    settings.body = JSON.stringify(data);
+  }
+  fetch(url, settings)
   .then(res => res.json())
   .then(res => cb(res))
   .catch(err => {
-    console.log('[searchBooks] err=', err);
+    console.log(`[fetchJSON] url=${url} method=${method} err=`, err);
     cb([]);
-  });
+  })
+}
+
+function searchBooks(query, cb) {
+  fetchJSON(`/api/googlebooks?q=${query}`, cb);
 }
 
 function getBooks(cb) {
-  fetch('/api/books')
-  .then(res => res.json())
-  .then(res => cb(res))
-  .catch(err => {
-    console.log('[getBooks] err=', err);
-    cb([]);
-  });
+  fetchJSON('/api/books', cb);
 }
 
-const noop = function(val){}; // do nothing.
+const noop = (val) => { }; // do nothing.
 
 function deleteBook(id, cb = noop) {
-  let settings = {
-    method: 'delete',
-    headers: { 'Content-Type': 'application/json' }
-  }
-  fetch(`/api/books/${id}`, settings)
-  .then(res => res.json())
-  .then(res => cb(res))
-  .catch(err => {
-    console.log('[deleteBook] err=', err);
-    cb([]);
-  });
+  fetchJSON(`/api/books/${id}`, cb, 'delete');
 }
 
 function saveBook(bookInfo, cb = noop) {
-  let settings = {
-    method: 'post',
-    headers: { 'Content-Type': 'application/json' },
-    body: JSON.stringify(bookInfo)
-  }
-  fetch('/api/books', settings)
-  .then(res => res.json())
-  .then(res => cb(res))
-  .catch(err => {
-    console.log('[saveBook] err=', err);
-    cb([]);
-  });
+  fetchJSON('/api/books', cb, 'post', bookInfo);
 }
 
 export { searchBooks, getBooks, deleteBook, saveBook };
